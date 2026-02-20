ff767e8300c7f301b0ac23a31034bd9f9714382d
Aoife Casey <20117299@setu.ie>
2026-02-20 01:32:21 +0000

    Changes now being deployed to GitHub and Render

--- controllers/start.js
+++ controllers/start.js
@@ -1,11 +1,19 @@
-import logger from "../utils/logger.js";
-
 'use strict';
+
+import logger from "../utils/logger.js";
+import appStore from "../models/app-store.js";
 
 const start = {
   createView(request, response) {
     logger.info("Start page loading!");
-    response.send("Welcome to Aoife's Playlist app!");   
+    
+    const viewData = {
+      title: "Welcome to the Playlist app!",
+      info: appStore.getAppInfo()
+    };
+    
+    //logger.debug(viewData);
+    response.render('start', viewData);   
   },
 };
 
