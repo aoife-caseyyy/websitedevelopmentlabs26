2a1e20fe22bac61bd521bdd8cd16c6ca5181c5f5
Aoife Casey <20117299@setu.ie>
2026-04-15 11:43:56 +0100

    User authentication added

--- routes.js
+++ routes.js
@@ -9,8 +9,9 @@ import start from './controllers/start.js';
 import dashboard from './controllers/dashboard.js';
 import about from './controllers/about.js';
 import playlist from './controllers/playlist.js';
+import accounts from './controllers/accounts.js';
 
-router.get('/', start.createView);
+router.get('/start', start.createView);
 router.get('/dashboard', dashboard.createView);
 router.get('/about', about.createView);
 router.get('/playlist/:id', playlist.createView);
@@ -19,11 +20,16 @@ router.get('/dashboard/deleteplaylist/:id', dashboard.deletePlaylist);
 router.get('/stats', stats.createView);
 router.get('/searchCategory', dashboard.createView);
 router.get('/sortData', dashboard.createView);
-
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
 router.get('/error', (request, response) => response.status(404).end('Page not found.'));
 
 router.post('/playlist/:id/addsong', playlist.addSong);
 router.post('/dashboard/addplaylist', dashboard.addPlaylist);
 router.post('/playlist/:id/updatesong/:songid', playlist.updateSong);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
 
 export default router;
