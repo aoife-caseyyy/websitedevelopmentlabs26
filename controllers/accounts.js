b88dd3a44eb49c788266e506abae22e0a28b349d
Aoife Casey <20117299@setu.ie>
2026-04-23 17:00:35 +0100

    User authentication added

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -49,7 +49,7 @@ const accounts = {
   //authenticate function to check user credentials and either render the login page again or the start page.
   authenticate(request, response) {
     const user = userStore.getUserByEmail(request.body.email);
-    if (user) {
+    if (user && user.password === request.body.password) {
       response.cookie('playlist', user.email);
       logger.info('logging in' + user.email);
       response.redirect('/start');
